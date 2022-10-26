31bd00f726892bae4d1062a9d752a612260b7ed4
Janez Lapajne <lapajne.janez@gmail.com>
2022-10-26 14:42:47 +0200

    optimization of tradition models implemented

---
+++ models_trad/optimizer.py
@@ -0,0 +1,189 @@
+import os
+import pickle
+from abc import abstractmethod
+from types import SimpleNamespace
+
+import numpy as np
+from ray import tune
+from ray.air import session
+from ray.air.config import RunConfig, ScalingConfig
+from ray.tune.schedulers import AsyncHyperBandScheduler, HyperBandScheduler
+from ray.tune.search.hyperopt import HyperOptSearch
+from rich.progress import track
+from sklearn.metrics import (
+    accuracy_score,
+    balanced_accuracy_score,
+    classification_report,
+    mean_absolute_error,
+)
+from sklearn.model_selection import cross_val_score
+from sklearn.utils import shuffle
+
+
+class BaseOptimizer:
+    def __init__(self, model, config):
+        self.logger = config.get_logger("trainer", verbosity=2)
+        self.model = model
+        self.mode, self.scoring = config["score"].split()
+        self.tuned_params = self._modify_params(config)
+        self.debug = config["debug"]
+        self.num_samples = config["num_samples"]
+        self.name = config["name"]
+        self.save_dir = config.save_dir
+        self.data = None
+
+    def optimize(self):
+        if self.data is None:
+            raise Exception("use self.load_data() before self.optimize()")
+        if self.debug:
+            print("Not implemented.")
+        else:
+            self._debug_false()
+
+    def _debug_false(self):
+        analysis = self.perform_search()
+        train_report = self.create_train_report(analysis)
+        self.model.set_params(**analysis.best_config)
+        self.model.fit(self.data.X_train, self.data.y_train)
+        self.save_model(self.model)
+        self.save_report(train_report, "report_train.txt")
+
+    def save_model(self, model):
+        save_path = os.path.join(self.save_dir, "model.pkl")
+        with open(save_path, "wb") as f:
+            pickle.dump(model, f)
+
+    def load_model(self):
+        load_path = os.path.join(self.save_dir, "model.pkl")
+        with open(load_path, "rb") as f:
+            model = pickle.load(f)
+            print(model)
+        return model
+
+    def save_report(self, report, name_txt):
+        save_path = os.path.join(self.save_dir, name_txt)
+        with open(save_path, "w") as text_file:
+            text_file.write(report)
+
+    @staticmethod
+    def _modify_params(config):
+        tuned_parameters = config["tuned_parameters"]
+
+        for method_name in tuned_parameters:
+            temp = tuned_parameters[method_name]
+            if temp[0] == "CHOICE":
+                temp.pop(0)
+                tuned_parameters[method_name] = tune.choice(temp)
+            elif len(temp) == 3 and temp[0] == "LOG_UNIFORM":
+                tuned_parameters[method_name] = tune.loguniform(temp[1], temp[2])
+            elif len(temp) == 3 and temp[0] == "RAND_INT":
+                tuned_parameters[method_name] = tune.randint(temp[1], temp[2])
+            else:
+                raise Exception("Parameters not configured properly")
+
+        return tuned_parameters
+
+    def create_train_report(self, analysis):
+        """Should return report from training"""
+        return "Train report not configured."
+
+    @abstractmethod
+    def load_data(self):
+        """Load of data implemented here"""
+        raise NotImplementedError
+
+    @abstractmethod
+    def perform_search(self):
+        """Seach of hyperparameters implemented here"""
+        raise NotImplementedError
+
+    @abstractmethod
+    def trainable(self):
+        """Should evaluate objective function used in perform search"""
+        raise NotImplementedError
+
+
+class OptimizerClassification(BaseOptimizer):
+    def __init__(self, model, data_loader, valid_data_loader, validator, config):
+        super().__init__(model, config)
+        self.data_loader = data_loader
+        self.valid_data_loader = valid_data_loader
+        self.validator = validator
+        self.best_config = None
+
+    def load_data(self):
+        X_train, y_train = self.convert_images_to_1d(self.data_loader)
+        X_valid, y_valid = self.convert_images_to_1d(self.valid_data_loader)
+        # create data structure i.e.: self.data.X_train, self.data.y_train, etc.
+        self.data = SimpleNamespace(
+            X_train=X_train, y_train=y_train, X_valid=X_valid, y_valid=y_valid
+        )
+        self.data_loader = None
+        self.valid_data_loader = None
+
+    def convert_images_to_1d(self, data_loader):
+        if data_loader is None:
+            return None, None
+        X, y = [], []
+        for images, targets in track(data_loader, description="Converting images..."):
+            # average spatial dimension of images
+            signatures = images.mean((2, 3)).numpy()
+            targets = targets.numpy()
+            [X.append(sig) for sig in signatures]
+            [y.append(tar) for tar in targets]
+        return X, y
+
+    def perform_search(self):
+        search_alg = HyperOptSearch()
+        scheduler = HyperBandScheduler()
+        tune_config = tune.TuneConfig(
+            mode=self.mode,
+            search_alg=search_alg,
+            scheduler=scheduler,
+            num_samples=self.num_samples,
+        )
+        run_config = RunConfig(
+            name=self.name,
+        )
+        tuner = tune.Tuner(
+            trainable=self.trainable,
+            param_space=self.tuned_params,
+            tune_config=tune_config,
+            run_config=run_config,
+        )
+        results = tuner.fit()
+        self.best_config = results.get_best_result(metric=self.scoring, mode=self.mode).config
+        return results
+
+    def trainable(self, config):
+        score = self.objective(config)
+        session.report({self.scoring: score, "_metric": score})
+
+    def objective(self, config):
+        self.model.set_params(**config)
+        score = cross_val_score(
+            self.model,
+            self.data.X_train,
+            self.data.y_train,
+            cv=self.validator,
+            error_score=0,
+            n_jobs=1,
+            pre_dispatch=1,
+            scoring=self.scoring,
+        )
+        return score.mean()
+
+    def create_train_report(self, results):
+        print(results.get_dataframe())
+        print(self.best_config)
+
+        quit()
+        return ""
+        # train_report = f" Best hyperparameters found were: {results.best_config}"
+        # train_report += f"\n\n Best results all: {results.best_result}"
+        # train_report += f"\n\n Best accuracy: {results.best_result['mean_accuracy']}\n"
+        # print(train_report)
+
+        # self.save_report(str(results.best_result["mean_accuracy"]), "accuracy_train.txt")
+
+        # return train_report
---
+++ train_trad.py
@@ -0,0 +1,74 @@
+import argparse
+import collections
+import glob
+
+import ray
+import sklearn.model_selection as model_selection_
+
+import data_loader.data_loaders as data_loaders_
+import models_trad.model as models_
+import models_trad.optimizer as optimizers_
+from utils.parse_config import ParseConfig
+from utils.utils import read_json
+
+
+def main(config):
+    ray.init()
+    logger = config.get_logger("train")
+
+    # setup data_loader instances
+    data_loader = config.init_obj("data_loader", data_loaders_)
+    valid_data_loader = data_loader.split_validation()
+
+    # initialize model and print to console
+    model = config.init_obj("model", models_).create()
+    logger.info(model)
+
+    # init validator and optimizer
+    validator = config.init_obj("validation", model_selection_)
+    Optimizer = config.import_module("optimizer", optimizers_)
+
+    optimizer = Optimizer(
+        model=model,
+        data_loader=data_loader,
+        valid_data_loader=valid_data_loader,
+        validator=validator,
+        config=config,
+    )
+    optimizer.load_data()
+    optimizer.optimize()
+
+
+if __name__ == "__main__":
+
+    args = argparse.ArgumentParser(description="Train traditional model")
+    args.add_argument(
+        "-c",
+        "--config",
+        default=None,
+        type=str,
+        help="config file path (default: None)",
+    )
+    args.add_argument(
+        "-r",
+        "--resume",
+        default=None,
+        type=str,
+        help="path to latest checkpoint (default: None)",
+    )
+    args.add_argument(
+        "-d",
+        "--device",
+        default=None,
+        type=str,
+        help="indices of GPUs to enable (default: all)",
+    )
+
+    # custom cli options to modify configuration from default values given in json file.
+    CustomArgs = collections.namedtuple("CustomArgs", "flags type target")
+    options = [
+        CustomArgs(["--lr", "--learning_rate"], type=float, target="optimizer;args;lr"),
+        CustomArgs(["--bs", "--batch_size"], type=int, target="data_loader;args;batch_size"),
+    ]
+    config = ParseConfig.from_args(args, options)
+    main(config)
