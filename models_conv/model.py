84bc509a7701a07f481fbf449a41daadac4d87f3
Remotesensing <remotesensing@kis.si>
2023-01-29 14:54:47 +0100

    implemented resnet architecture -- removed old one

--- models_conv/model.py
+++ models_conv/model.py
@@ -1,6 +1,7 @@
 from abc import abstractmethod
 
 import numpy as np
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -28,48 +29,265 @@ class BaseModel(nn.Module):
         return super().__str__() + "\nTrainable parameters: {}".format(params)
 
 
+class BasicConv(nn.Module):
+    def __init__(
+        self,
+        channels_in,
+        channels_out,
+        kernel_size=3,
+        stride=1,
+        padding=0,
+        dilation=1,
+        groups=1,
+        bias=True,
+        normalization=True,
+        activation=nn.ReLU(True),
+        act_first=True,
+    ):
+        super(BasicConv, self).__init__()
 
-class ConvNet(BaseModel):
-    def __init__(self):
-        super().__init__()
-        self.conv1 = self._conv_block(373, 128)
-        self.conv2 = self._conv_block(128, 64)
-        self.conv3 = self._conv_block(64, 32)
-        self.conv4 = self._conv_block(32, 16)
+        basic_conv = [
+            nn.Conv2d(
+                channels_in,
+                channels_out,
+                kernel_size=kernel_size,
+                stride=stride,
+                padding=padding,
+                dilation=dilation,
+                groups=groups,
+                bias=bias,
+            )
+        ]
 
-        self.flatten = nn.Flatten(1)
-
-        self.fc1 = self._fc_block(256, 64, activation="relu")
-        self.fc2 = self._fc_block(64, 1, activation="sigmoid")
-
-    def _conv_block(self, in_channels, out_channels):
-        conv_block = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, 3, padding=1),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(),
-            # nn.Dropout(0.1),
-            nn.MaxPool2d(2, 2),
-        )
-        return conv_block
-
-    def _fc_block(self, in_channels, out_channels, activation="relu"):
-        if activation == "relu":
-            activation = nn.ReLU()
-        elif activation == "sigmoid":
-            activation = nn.Sigmoid()
-        fc_block = nn.Sequential(
-            nn.Linear(in_channels, out_channels),
-            # nn.Dropout(0.1),
-            activation,
-        )
-        return fc_block
+        if activation and act_first:
+            basic_conv.append(activation)
+        if normalization:
+            basic_conv.append(nn.BatchNorm2d(channels_out))
+        if activation and not act_first:
+            basic_conv.append(activation)
+        self.body = nn.Sequential(*basic_conv)
 
     def forward(self, x):
+        return self.body(x)
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(
+        self,
+        inplanes,
+        planes,
+        stride=1,
+        downsample=None,
+        groups=1,
+        base_width=64,
+        dilation=1,
+        normalization=True,
+        activation=nn.ReLU(),
+    ):
+        super(BasicBlock, self).__init__()
+        if groups != 1 or base_width != 64:
+            raise ValueError("BasicBlock only supports groups=1 and base_width=64")
+        if dilation > 1:
+            raise NotImplementedError("Dilation > 1 not supported in BasicBlock")
+        # Both self.conv1 and self.downsample layers downsample the input when stride != 1
+
+        self.conv1 = BasicConv(
+                        inplanes,
+                        planes,
+                        kernel_size=3,
+                        stride=stride,
+                        padding=1,
+                        groups=1,
+                        bias=False,
+                        dilation=1,
+                        normalization=normalization,
+                        activation=False,
+                    )
+
+        self.act = activation
+        self.conv2 = BasicConv(
+                        planes,
+                        planes,
+                        kernel_size=3,
+                        stride=1,
+                        padding=1,
+                        groups=1,
+                        bias=False,
+                        dilation=1,
+                        normalization=normalization,
+                        activation=False,
+                    )
+
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.act(out)
+        out = self.conv2(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.act(out)
+
+        return out
+
+
+class ConvNet(BaseModel):
+    def __init__(
+        self,
+        block=BasicBlock,
+        layers=[2, 2, 2, 2],
+        img_channels=373,  # number of spectral bands
+        res_channels=64,
+        num_classes=1,
+        zero_init_residual=False,
+        groups=1,
+        width_per_group=64,
+        replace_stride_with_dilation=None,
+        normalization=True,
+        activation=nn.ReLU(),
+    ):
+        super(ConvNet, self).__init__()
+
+        self.inplanes = res_channels
+        self.dilation = 1
+        if replace_stride_with_dilation is None:
+            # each element in the tuple indicates if we should replace
+            # the 2x2 stride with a dilated convolution instead
+            replace_stride_with_dilation = [False, False, False]
+        if len(replace_stride_with_dilation) != 3:
+            raise ValueError(
+                "replace_stride_with_dilation should be None "
+                "or a 3-element tuple, got {}".format(replace_stride_with_dilation)
+            )
+        self.groups = groups
+        self.base_width = width_per_group
+        self.normalization = normalization
+        self.activation = activation
+
+        self.conv1 = BasicConv(
+            img_channels,
+            self.inplanes,
+            kernel_size=7,
+            stride=2,
+            padding=3,
+            bias=False,
+            normalization=self.normalization,
+        )
+
+        self.act = activation
+        self.maxpool = nn.MaxPool2d(3, stride=2, padding=1, dilation=1)
+        self.avgpool = nn.AdaptiveAvgPool2d(1)
+
+        self.layer1 = self._make_layer(block, res_channels, layers[0])
+        self.layer2 = self._make_layer(
+            block, res_channels * 2, layers[1], stride=2, dilate=replace_stride_with_dilation[0]
+        )
+        self.layer3 = self._make_layer(
+            block, res_channels * 4, layers[2], stride=2, dilate=replace_stride_with_dilation[1]
+        )
+        self.layer4 = self._make_layer(
+            block, res_channels * 8, layers[3], stride=2, dilate=replace_stride_with_dilation[2]
+        )
+
+        self.fc = nn.Sequential(
+            nn.Linear(res_channels * 8 * block.expansion, num_classes),
+            nn.Sigmoid()
+        )
+
+        for m in self.modules():
+            if isinstance(m, (nn.Conv1d, nn.Conv2d, nn.Conv3d)):
+                nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
+            elif isinstance(
+                m,
+                (
+                    (
+                        nn.BatchNorm1d,
+                        nn.BatchNorm2d,
+                        nn.BatchNorm3d,
+                        nn.GroupNorm,
+                        nn.LayerNorm,
+                        nn.InstanceNorm1d,
+                        nn.InstanceNorm2d,
+                        nn.InstanceNorm3d,
+                    )
+                ),
+            ):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+        if zero_init_residual:
+            for m in self.modules():
+                if isinstance(m, BasicBlock):
+                    nn.init.constant_(m.bn2.weight, 0)
+
+    def _make_layer(self, block, planes, blocks, stride=1, dilate=False):
+        downsample = None
+        previous_dilation = self.dilation
+        if dilate:
+            self.dilation *= stride
+            stride = 1
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = BasicConv(
+                            self.inplanes,
+                            planes * block.expansion,
+                            kernel_size=1,
+                            stride=stride,
+                            bias=False,
+                            normalization=self.normalization,
+                            activation=False,
+                        )
+
+        layers = []
+        layers.append(
+            block(
+                self.inplanes,
+                planes,
+                stride,
+                downsample,
+                self.groups,
+                self.base_width,
+                previous_dilation,
+                normalization=self.normalization,
+                activation=self.activation,
+            )
+        )
+        self.inplanes = planes * block.expansion
+        for _ in range(1, blocks):
+            layers.append(
+                block(
+                    self.inplanes,
+                    planes,
+                    groups=self.groups,
+                    base_width=self.base_width,
+                    dilation=self.dilation,
+                    normalization=self.normalization,
+                    activation=self.activation,
+                )
+            )
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        # See note [TorchScript super()]
         x = self.conv1(x)
-        x = self.conv2(x)
-        x = self.conv3(x)
-        x = self.conv4(x)
-        x = self.flatten(x)
-        x = self.fc1(x)
-        x = self.fc2(x)
+        x = self.act(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+
         return x
