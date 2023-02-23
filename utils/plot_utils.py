5e32378459a4bb033ee1a6f72d3843ac931a73d8
Janez Lapajne <lapajne.janez@gmail.com>
2023-02-23 18:48:16 +0100

    implemented signature plotting

--- utils/plot_utils.py
+++ utils/plot_utils.py
@@ -2,6 +2,8 @@ import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.collections import LineCollection
+from matplotlib.lines import Line2D
+from sklearn import preprocessing
 
 from configs import configs
 from utils.tools import calculate_roc_curve, find_signal_peaks
@@ -35,25 +37,29 @@ def plot_relavant_features(relevances):
     ax.set_yticklabels([])
 
 
-def plot_relevances_amplitudes(relevances):
+def plot_relevances_amplitudes(relevances, title=""):
     y = np.zeros_like(configs.BANDS_ORIGINAL)
     # remove the pre-defined noisy bands and assign values to the remaining places
     y[np.delete(np.arange(len(configs.BANDS_ORIGINAL)), configs.NOISY_BANDS)] = relevances
     x = np.array(configs.BANDS_ORIGINAL)
 
-    fig, ax = plt.subplots(1, 1, sharex=True, sharey=True, figsize=(10, 8))
+    fig, ax = plt.subplots(1, 1, sharex=True, sharey=True, figsize=(8, 7), dpi=100)
     plt.plot(x, y, c="gray", linewidth=0.3, zorder=1)
     plt.scatter(x, y, c=y, s=3, cmap="viridis", zorder=2)
 
     ax.set_xlim(x.min(), x.max())
     ax.set_ylim(0, 1.2)
     ax.set_xlabel("Wavelength [nm]")
-    ax.xaxis.label.set_size(12)
     ax.set_ylabel("Relevance")
-    ax.yaxis.label.set_size(12)
+
+    plt.tick_params(labelsize=22)
+    ax.set_title(title, fontsize=24)
+    ax.set_ylabel("Sensitivity", fontsize=24)
+    ax.set_xlabel("1-Specificity", fontsize=24)
+
     peak_heights_dict, peak_indexes = find_signal_peaks(y)
     [plt.text(x[idx], peak_heights_dict[idx], int(x[idx])) for idx in peak_indexes]
-    # plt.axhline(y=1.5, color="r", linestyle="-")
+    # plt.axhline(y=1, color="r", linestyle="--", alpha=0.6)
 
 
 def plot_roc_curves(data_dfs, title=""):
@@ -90,3 +96,66 @@ def plot_roc_curves(data_dfs, title=""):
     ax.spines["right"].set_linewidth(0)
     ax.spines["top"].set_linewidth(0)
     # plt.savefig("AUC.pdf", format="pdf")
+
+
+def plot_signatures(
+    signatures, labels, *, average=True, title="", x_label="", y_label="", vertical_lines=None
+):
+    transformer = preprocessing.LabelEncoder()
+    transformer.fit(labels)
+    labels = transformer.transform(labels)
+    x_scat = configs.BANDS
+
+    if average:
+        signatures_mean = []
+        signatures_dev = []
+        labels_mean = []
+        for label in np.unique(labels):
+            indices = np.where(labels == label)
+            sig_mean = np.mean(signatures[indices], axis=0)
+            sig_std = np.std(signatures[indices], axis=0)
+
+            signatures_mean.append(sig_mean)
+            signatures_dev.append(sig_std)
+            labels_mean.append(label)
+
+        signatures = signatures_mean
+        labels = labels_mean
+
+    fig = plt.figure()
+    fig.set_figheight(5)
+    fig.set_figwidth(15)
+
+    ax = fig.add_subplot(1, 1, 1)
+    ax.set_title(title, fontsize=24)
+    ax.set_ylabel(y_label, fontsize=24)
+    ax.set_xlabel(x_label, fontsize=24)
+    ax.tick_params(axis="both", which="major", labelsize=22)
+    ax.tick_params(axis="both", which="minor", labelsize=22)
+    ax.set_ylim([0, 1])
+    ax.spines["top"].set_visible(False)
+    ax.spines["right"].set_visible(False)
+
+    no_colors = len(np.unique(labels))
+    colors = ["g", "r"]
+
+    assert len(signatures) == len(labels)
+
+    for obj in range(len(signatures)):
+        ax.plot(x_scat, signatures[obj], color=colors[labels[obj]], alpha=0.6)
+        if average:
+            std_up = signatures[obj] + signatures_dev[obj]
+            std_down = signatures[obj] - signatures_dev[obj]
+            ax.fill_between(x_scat, std_down, std_up, color=colors[labels[obj]], alpha=0.3)
+
+    # also plot vertical lines
+    if vertical_lines is not None:
+        for v_line in vertical_lines:
+            ax.axvline(x=x_scat[v_line], color="k", linestyle="--")
+
+    custom_lines = []
+    for idx in range(no_colors):
+        custom_lines.append(Line2D([0], [0], color=colors[idx], lw=2))
+
+    labels = transformer.inverse_transform(list(range(no_colors)))
+    ax.legend(custom_lines, [str(num) for num in labels], fontsize=22)
