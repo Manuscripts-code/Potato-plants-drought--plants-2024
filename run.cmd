89cb83454251ec228a236237e995adc1c8a01b28
Remotesensing <remotesensing@kis.si>
2023-03-07 00:40:35 +0100

    added stratrify + l1 penalty configs

--- run.cmd
+++ run.cmd
@@ -9,6 +9,13 @@
 @REM python main.py -c configs/krka/stratify/krka_stratify_5432.json -m train_test
 @REM python main.py -c configs/krka/stratify/krka_stratify_54321.json -m train_test
 
+@REM ___ Krka stratify configs + added l1 penalty(correct plant depentant split) ___
+@REM python main.py -c configs/krka/stratify_penalty/krka_stratify_5.json -m train_test
+@REM python main.py -c configs/krka/stratify_penalty/krka_stratify_4.json -m train_test
+@REM python main.py -c configs/krka/stratify_penalty/krka_stratify_3.json -m train_test
+@REM python main.py -c configs/krka/stratify_penalty/krka_stratify_2.json -m train_test
+@REM python main.py -c configs/krka/stratify_penalty/krka_stratify_1.json -m train_test
+@REM python main.py -c configs/krka/stratify_penalty/krka_stratify_5432.json -m train_test
 
 @REM ___ Krka random (incorrect plant independat split) ___
 @REM python main.py -c configs/krka/random/krka_random_5.json -m train_test
